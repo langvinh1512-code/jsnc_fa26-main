926a317ef5fbbb2e8b508dd8ed1049fd60363b8e
langvinh1512-code <langvinh1512@gmail.com>
2026-09-15 15:56:44 +0700

    lab 1 hoan thanh

---
+++ lab1.js
@@ -0,0 +1,10 @@
+console.log("day la lab1");
+let name = "Quang Vinh";
+let age = 18;
+let address = "Lạng Sơn";
+let isStudent = true;
+
+console.log("Họ tên:", name);
+console.log("Tuổi:", age);
+console.log("Địa chỉ:", address);
+console.log("Sinh viên:", isStudent);
